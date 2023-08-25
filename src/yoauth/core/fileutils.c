ff00fae99e99b55173d83e3ab01f6eef94445602
Muggle Wei <mugglewei@gmail.com>
2023-08-25 10:13:51 +0800

    update core/fileutils

--- src/yoauth/core/fileutils.c
+++ src/yoauth/core/fileutils.c
@@ -26,6 +26,11 @@ unsigned char *yoauth_fileutils_load_totp(const char *filepath,
 		goto load_exit;
 	}
 
+	if (fread(ciphertext, 1, numbytes, fp) != (unsigned long)numbytes) {
+		LOG_ERROR("failed read bytes from file");
+		goto load_exit;
+	}
+
 	plaintext = yoauth_decrypt(ciphertext, numbytes, password, outlen);
 	if (plaintext == NULL) {
 		LOG_ERROR("failed decrypt TOTP file");
@@ -68,6 +73,67 @@ bool yoauth_fileutils_store_totp(const char *filepath, const char *password,
 								 unsigned char *plaintext,
 								 unsigned long numbytes)
 {
-	// TODO:
+	unsigned char *ciphertext = NULL;
+	FILE *fp = NULL;
+
+	char dirpath[MUGGLE_MAX_PATH];
+	if (muggle_path_dirname(filepath, dirpath, sizeof(dirpath)) != 0) {
+		LOG_ERROR("failed get dir path: %s", filepath);
+		goto err_store;
+	}
+
+	if (!muggle_path_exists(dirpath)) {
+		if (muggle_os_mkdir(dirpath) != 0) {
+			LOG_ERROR("failed create directory: %s", dirpath);
+			goto err_store;
+		}
+	}
+
+	unsigned long outlen = 0;
+	ciphertext = yoauth_encrypt(plaintext, numbytes, password, &outlen);
+	if (ciphertext == NULL) {
+		LOG_ERROR("failed encrypt TOTP content");
+		goto err_store;
+	}
+
+	char tmp_path[MUGGLE_MAX_PATH];
+	memset(tmp_path, 0, sizeof(tmp_path));
+	snprintf(tmp_path, sizeof(tmp_path), "%s.tmp", filepath);
+
+	fp = fopen(tmp_path, "wb");
+	if (fp == NULL) {
+		LOG_ERROR("failed open file to write: %s", tmp_path);
+		goto err_store;
+	}
+
+	if (fwrite(ciphertext, 1, outlen, fp) != outlen) {
+		LOG_ERROR("failed write bytes");
+		goto err_store;
+	}
+
+	fclose(fp);
+	fp = NULL;
+
+	if (ciphertext) {
+		free(ciphertext);
+		ciphertext = NULL;
+	}
+
+	if (muggle_os_rename(tmp_path, filepath) != 0) {
+		LOG_ERROR("failed rename file");
+		goto err_store;
+	}
+
 	return true;
+
+err_store:
+	if (fp) {
+		fclose(fp);
+	}
+
+	if (ciphertext) {
+		free(ciphertext);
+	}
+
+	return false;
 }
--- test/fileutils/test_fileutils.c
+++ test/fileutils/test_fileutils.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "yoauth/core/crypt.h"
+#include "yoauth/core/core.h"
 #include "openssl/evp.h"
 #include "openssl/rand.h"
 #include "muggle/c/muggle_c.h"
@@ -14,7 +14,57 @@ void tearDown()
 
 void test_fileutils()
 {
-	// TODO:
+	int iret = 0;
+
+	// prepare filepath
+	char exepath[MUGGLE_MAX_PATH];
+	iret = muggle_os_process_path(exepath, sizeof(exepath));
+	TEST_ASSERT_EQUAL_INT32(iret, 0);
+
+	char dirpath[MUGGLE_MAX_PATH];
+	iret = muggle_path_dirname(exepath, dirpath, sizeof(dirpath));
+	TEST_ASSERT_EQUAL_INT32(iret, 0);
+
+	char filepath[MUGGLE_MAX_PATH];
+	iret = muggle_path_join(dirpath, "test_fileutils.data", filepath,
+							sizeof(filepath));
+	TEST_ASSERT_EQUAL_INT32(iret, 0);
+
+	// prepare data
+	int cnt_totp_data = 100;
+	unsigned char *plaintext = (unsigned char *)malloc(
+		sizeof(yoauth_head_t) + cnt_totp_data * sizeof(yoauth_data_totp_t));
+	TEST_ASSERT_NOT_NULL(plaintext);
+
+	yoauth_head_t *hdr = (yoauth_head_t *)plaintext;
+	memcpy(hdr, YOAUTH_DATA_HEAD_MAGIC, 4);
+	hdr->ver = YOAUTH_DATA_VERSION;
+	hdr->datalen = cnt_totp_data * sizeof(yoauth_data_totp_t);
+	hdr->datatype = YOAUTH_DATA_TYPE_TOTP;
+
+	yoauth_data_totp_t *datas = (yoauth_data_totp_t *)(hdr + 1);
+	for (int i = 0; i < cnt_totp_data; i++) {
+		RAND_bytes((unsigned char *)&datas[i], sizeof(yoauth_data_totp_t));
+	}
+
+	// write into file
+	const char *password = "hello123666";
+	bool bret = yoauth_fileutils_store_totp(
+		filepath, password, plaintext, sizeof(yoauth_head_t) + hdr->datalen);
+	TEST_ASSERT_TRUE(bret);
+
+	unsigned long plaintext_len = 0;
+	unsigned char *ret_plaintext =
+		yoauth_fileutils_load_totp(filepath, password, &plaintext_len);
+	TEST_ASSERT_NOT_NULL(ret_plaintext);
+	TEST_ASSERT_EQUAL_UINT32(plaintext_len,
+							 sizeof(yoauth_head_t) + hdr->datalen);
+	int n = memcmp(ret_plaintext, plaintext, plaintext_len);
+	TEST_ASSERT_EQUAL_INT(n, 0);
+
+	// cleanup
+	free(ret_plaintext);
+	free(plaintext);
 }
 
 int main()
@@ -25,4 +75,3 @@ int main()
 
 	return UNITY_END();
 }
-
